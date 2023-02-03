6920385b4576e23282bc2e41dc1f851a30a29638
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2023-02-03 11:34:07 +0800

    [feat] gallery

--- hexo/source/album/index.md
+++ hexo/source/album/index.md
@@ -4,3 +4,6 @@ comments: false
 description: 更新日期：2023年2月2日
 categories: [相册]
 ---
+
+
+<div id="0xje8cj39d0e" class="justified-gallery"></div>
--- hexo/source/main.js
+++ hexo/source/main.js
@@ -154,3 +154,117 @@ loadjQuery(() => {
 // );
 
 
+
+const InfiniteScrollGallery = {
+  requestAPI: (url, callback, timeout) => {
+    let retryTimes = 5;
+    function request() {
+      return new Promise((resolve, reject) => {
+        let status = 0; // 0 等待 1 完成 2 超时
+        let timer = setTimeout(() => {
+          if (status === 0) {
+            status = 2;
+            timer = null;
+            reject('请求超时');
+            if (retryTimes == 0) {
+              timeout();
+            }
+          }
+        }, 5000);
+        fetch(url).then(function(response) {
+          if (status !== 2) {
+            clearTimeout(timer);
+            resolve(response);
+            timer = null;
+            status = 1;
+          }
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Network response was not ok.');
+        }).then(function(data) {
+          retryTimes = 0;
+          callback(data);
+        }).catch(function(error) {
+          if (retryTimes > 0) {
+            retryTimes -= 1;
+            setTimeout(() => {
+              request();
+            }, 5000);
+          } else {
+            timeout();
+          }
+        });
+      });
+    }
+    request();
+  },
+  layoutDiv: (cfg) => {
+    const el = $(cfg.el)[0];
+    $(el).append('<div class="loading-wrap"><svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-dasharray="60" stroke-dashoffset="60" stroke-opacity=".3" d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0"/></path><path stroke-dasharray="15" stroke-dashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></g></svg></div>');
+    InfiniteScrollGallery.requestAPI(cfg.api, function(data) {
+      $(el).find('.loading-wrap').remove();
+      // 初始化
+      // $("#" + cfg.id).justifiedGallery(
+      //   {
+      //     lastRow : 'left', 
+      //     captions: false,
+      //     margins : 3,
+      //     border: -1
+      //   }
+      // )
+      // 挑选出图片
+      var pictures_list = [];
+      data.forEach((item, i) => {
+        console.log(item);
+        console.log(i);
+        console.log(item.name);
+        temp_name = item.name.split('.');
+        console.log(temp_name);
+        if (temp_name[temp_name.length-1] == 'jpg' || temp_name[temp_name.length-1] == 'webp' || temp_name[temp_name.length-1] == 'png'){
+          pictures_list.push(item.name)
+        }
+      });
+      pictures_list.forEach((picture, i) => {
+        var picname = picture.split('.')[0];
+        var href = cfg.cdn + picture;
+        var img = `<img no-lazy src="${href}" alt="${picname}">`;
+        var el = '<a fancybox="true" data-fancybox="' + cfg.id + '" data-caption="'+picname+'" target="_blank" rel="noopener" href="'+href+'">';
+        el += img;
+        el += '</a>';
+        $("#" + cfg.id).append(el);
+      });
+      $("#" + cfg.id).justifiedGallery( 
+        {
+          lastRow : 'left', 
+          captions: false,
+          margins : 3,
+          border: -1
+        }
+      );
+    }, function() {
+      $(el).find('.loading-wrap svg').remove();
+      $(el).find('.loading-wrap').append('<svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke-dasharray="60" stroke-dashoffset="60" d="M12 3L21 20H3L12 3Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.5s" values="60;0"/></path><path stroke-dasharray="6" stroke-dashoffset="6" d="M12 10V14"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.2s" values="6;0"/></path></g><circle cx="12" cy="17" r="1" fill="currentColor" fill-opacity="0"><animate fill="freeze" attributeName="fill-opacity" begin="0.8s" dur="0.4s" values="0;1"/></circle></svg>');
+      $(el).find('.loading-wrap').addClass('error');
+    });
+  },  
+}
+
+
+
+loadCSS(gallery.justifiedGallery.css);
+loadjQuery(() => {
+  loadScript(gallery.justifiedGallery.js).then(()=>{
+    var cfg = new Object();
+    cfg.api = "https://api.github.com/repos/shaoyaoqian/shaoyaoqian.github.io/contents/hexo/source/img/album-zhoujielun?ref=source";
+    cfg.cdn = "https://raw.githubusercontent.com/shaoyaoqian/shaoyaoqian.github.io/source/hexo/source/img/album-zhoujielun/"
+    cfg.id  = "0xje8cj39d0e";
+    if (document.getElementById(cfg.id)) {
+      InfiniteScrollGallery.layoutDiv(cfg);
+    }
+
+  });
+});
+
+
+
